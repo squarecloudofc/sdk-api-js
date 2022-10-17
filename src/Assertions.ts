a9f5446a5b88f069a5ce230b6696c28f61687e22
João Gabriel Tonaco <64219777+bluee-js@users.noreply.github.com>
2022-10-17 19:07:18 -0300

    Update Assertions.ts

--- src/Assertions.ts
+++ src/Assertions.ts
@@ -1,3 +1,4 @@
+import { ReadStream } from 'fs';
 import z from 'zod';
 
 export function validateString(value: any): asserts value is string {
@@ -8,8 +9,10 @@ export function validateBoolean(value: any): asserts value is boolean {
   z.boolean().parse(value);
 }
 
-export function validatePathLike(value: any): asserts value is string | Buffer {
+export function validateCommitLike(
+  value: any
+): asserts value is string | ReadStream {
   z.string()
-    .or(z.custom((value) => value instanceof Buffer))
+    .or(z.custom((value) => value instanceof ReadStream))
     .parse(value);
 }
