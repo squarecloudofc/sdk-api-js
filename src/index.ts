86d96953487c96eccc68ad94d3d5a5f470f85b5a
João Gabriel Tonaco <64219777+bluee-js@users.noreply.github.com>
2022-10-17 19:20:21 -0300

    Assertions implemented everywhere

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { APIManager, SquareCloudAPIError } from './APIManager';
 import { Application } from './structures/Application';
 import { FullUser, User } from './structures/User';
+import { validateString } from './Assertions';
 
 export class SquareCloudAPI {
   static apiInfo = {
@@ -16,9 +17,7 @@ export class SquareCloudAPI {
    * @param apiKey - Your API Token (you can get it at [SquareCloud Dashboard](https://squarecloud.app/dashboard))
    */
   constructor(apiKey: string) {
-    if (typeof apiKey !== 'string') {
-      throw new TypeError('apiKey must be a string. Got ' + typeof apiKey);
-    }
+    validateString(apiKey);
 
     this.apiManager = new APIManager(apiKey);
   }
@@ -29,11 +28,9 @@ export class SquareCloudAPI {
    * @param userId - The user id, if not provided it will get your own information
    */
   async getUser(userId?: string): Promise<User> {
-    if (userId && typeof userId !== 'string') {
-      throw new TypeError('userId must be a string. Got ' + typeof userId);
-    }
+    validateString(userId);
 
-    const userData = (await this.apiManager.user(userId));
+    const userData = await this.apiManager.user(userId);
     const hasAccess = userData.user.email !== 'Access denied';
 
     return new (hasAccess ? FullUser : User)(this.apiManager, userData);
@@ -45,11 +42,9 @@ export class SquareCloudAPI {
    * @param appId - The application id, you must own the application
    */
   async getApplication(appId: string): Promise<Application> {
-    if (typeof appId !== 'string') {
-      throw new TypeError('appId must be a string. Got ' + typeof appId);
-    }
+    validateString(appId);
 
-    const { applications } = (await this.apiManager.user());
+    const { applications } = await this.apiManager.user();
     const appData = applications.find((app) => app.id === appId);
 
     if (!appData) {
