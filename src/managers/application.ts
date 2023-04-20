593b9ad31c1fed330ffb382cf50c6fad02f558a4
Gabriel <64219777+bluee-js@users.noreply.github.com>
2023-04-20 12:23:31 -0300

    new: Now it's possible to get all applications

--- src/managers/application.ts
+++ src/managers/application.ts
@@ -2,6 +2,8 @@ import FormData from 'form-data';
 import { readFile } from 'fs/promises';
 import { validatePathLike, validateString } from '../assertions';
 import Application from '../structures/application';
+import Collection from '../structures/collection';
+import { FullUser } from '../structures/user';
 import { APIResponse, UploadedApplicationResponse } from '../types';
 import APIManager from './api';
 
@@ -9,20 +11,28 @@ export default class ApplicationManager {
   constructor(private readonly apiManager: APIManager) {}
 
   /**
-   * Returns an application that you can manage or get information
+   * If the ID is provided, it will return an application that you can manage or get information
+   * If the ID is not provided, it will return a collection of applications
    *
    * @param appId - The application ID, you must own the application
    */
-  async get(appId: string) {
-    validateString(appId, 'APP_ID');
-
-    const { response: user } = await this.apiManager.user();
-    const data = user?.applications.find((app) => app.id === appId);
-
-    if (!data) {
+  async get(): Promise<Collection<string, Application> | undefined>;
+  async get(appId: string): Promise<Application | undefined>;
+  async get(
+    appId?: string
+  ): Promise<Application | Collection<string, Application> | undefined> {
+    const { response } = await this.apiManager.user();
+    if (!response) {
       return;
     }
-    return new Application(this.apiManager, data);
+
+    const { applications } = new FullUser(this.apiManager, response);
+
+    if (appId) {
+      validateString(appId, 'APP_ID');
+      return applications.get(appId);
+    }
+    return applications;
   }
 
   /**
