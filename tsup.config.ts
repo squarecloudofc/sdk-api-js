0d23e9d448396221633a5a894eb83750b524cd22
João Gabriel Tonaco User <joao.tonaco@proton.me>
2024-07-26 00:17:43 -0300

    chore: use tsup config in ts

---
+++ tsup.config.ts
@@ -0,0 +1,11 @@
+import type { Options } from "tsup";
+
+export default {
+	target: "es2020",
+	format: ["cjs", "esm"],
+	outDir: "lib",
+	splitting: false,
+	sourcemap: true,
+	clean: true,
+	dts: true,
+} satisfies Options;
