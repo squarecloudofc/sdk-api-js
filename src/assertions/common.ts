cb5b55086b9abe83ce0171122c505a6e657010a9
João Gabriel <joao.tonaco@proton.me>
2025-04-24 14:16:44 -0300

    refactor(assertions): simplify assertion logic and rename code to name

--- src/assertions/common.ts
+++ src/assertions/common.ts
@@ -4,25 +4,23 @@ export interface AssertionProps {
 	validate: (value: unknown) => boolean;
 	expect: string;
 	value: unknown;
-	code?: string;
+	name?: string;
 }
 
-export function assert({ validate, value, expect, code }: AssertionProps) {
-	try {
-		validate(value);
-	} catch {
-		throw new SquareCloudAPIError(
-			code ? `INVALID_${code}` : "VALIDATION_ERROR",
-			`Expect ${expect}, got ${typeof value}`,
-		);
+export function assert({ validate, value, expect, name }: AssertionProps) {
+	if (!validate(value)) {
+		const code = name ? `INVALID_${name}` : "VALIDATION_ERROR";
+		const message = `Expected ${expect}, got ${typeof value}`;
+
+		throw new SquareCloudAPIError(code, message);
 	}
 }
 
-export function makeAssertion<T>(
+export function makeAssertion(
 	expect: string,
 	validate: (value: unknown) => boolean,
 ) {
-	return (value: unknown, code?: string): asserts value is T => {
-		assert({ validate, value, expect, code });
+	return (value: unknown, name?: string) => {
+		assert({ validate, value, expect, name });
 	};
 }
--- src/assertions/literal.ts
+++ src/assertions/literal.ts
@@ -1,16 +1,16 @@
 import { makeAssertion } from "./common";
 
-export const assertString = makeAssertion<string>(
+export const assertString = makeAssertion(
 	"string",
 	(value) => typeof value === "string",
 );
 
-export const assertBoolean = makeAssertion<boolean>(
+export const assertBoolean = makeAssertion(
 	"boolean",
 	(value) => typeof value === "boolean",
 );
 
-export const assertPathLike = makeAssertion<string | Buffer>(
+export const assertPathLike = makeAssertion(
 	"string or Buffer",
 	(value) => typeof value === "string" || value instanceof Buffer,
 );
